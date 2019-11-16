96aae17e95a2e6fb2747d20b1099f4dd47d3c053
vladimir <nepeyvodava@gmail.com>
2019-11-16 23:02:00 +0300

    UIView: added extension

--- NASA/Helper/Extensions/UIViewExt.swift
+++ NASA/Helper/Extensions/UIViewExt.swift
@@ -6,4 +6,11 @@ extension UIView {
         views.forEach { self.addSubview($0) }
     }
     
+    func setRoundCorners(corners: UIRectCorner, radius: CGFloat) {
+        let path = UIBezierPath(roundedRect: bounds, byRoundingCorners: corners, cornerRadii: CGSize(width: radius, height: radius))
+        let mask = CAShapeLayer()
+        mask.path = path.cgPath
+        layer.mask = mask
+    }
+    
 }
