253ebb3f325127b0325db239eb638f2e7100bbfd
vladimir <nepeyvodava@gmail.com>
2019-11-16 19:23:06 +0300

    added GCD class

--- NASA/Service/GCD.swift
+++ NASA/Service/GCD.swift
@@ -1,9 +1,14 @@
-//
-//  GCD.swift
-//  NASA
-//
-//  Created by Владимир on 15/11/2019.
-//  Copyright © 2019 nva. All rights reserved.
-//
-
 import Foundation
+
+class GCD {
+    
+    static let searchQueue = DispatchQueue(label: "com.nva.search_queue")
+    static var currentSearchTask: DispatchWorkItem?
+    
+    static func newSearchTask(_ task: DispatchWorkItem, _ delay: Double = 2.0) {
+        currentSearchTask?.cancel()
+        currentSearchTask = task
+        searchQueue.asyncAfter(deadline: .now() + delay, execute: task)
+    }
+    
+}
