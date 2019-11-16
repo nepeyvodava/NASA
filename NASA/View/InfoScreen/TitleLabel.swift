7d06a0821dab1c9296680f219a92b06c3665725b
vladimir <nepeyvodava@gmail.com>
2019-11-16 19:27:35 +0300

    added custom label: TitleLabel

--- NASA/View/InfoScreen/TitleLabel.swift
+++ NASA/View/InfoScreen/TitleLabel.swift
@@ -1,21 +1,42 @@
-//
-//  TitleLabel.swift
-//  NASA
-//
-//  Created by Владимир on 15/11/2019.
-//  Copyright © 2019 nva. All rights reserved.
-//
-
 import UIKit
 
 class TitleLabel: UILabel {
+    
+    private var fromColor: UIColor = .white
+    private var toColor: UIColor = .black
 
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+    convenience init(_ from: UIColor, _ to: UIColor) {
+        self.init(frame: .zero)
+        fromColor = from
+        toColor = to
     }
-    */
+    
+    override public class var layerClass: AnyClass { CAGradientLayer.self }
+    
+    var gradientLayer: CAGradientLayer { layer as! CAGradientLayer }
+    
+    override func drawText(in rect: CGRect) {
+        let insets = UIEdgeInsets(top: 0, left: 5, bottom: 0, right: 5)
+        super.drawText(in: rect.inset(by: insets))
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        updateLocations()
+        updateColors()
+    }
 
+}
+
+
+private extension TitleLabel {
+    
+    func updateLocations() {
+        gradientLayer.locations = [0.0, 1.0]
+    }
+    
+    func updateColors() {
+        gradientLayer.colors = [fromColor.cgColor, toColor.cgColor]
+    }
+    
 }
