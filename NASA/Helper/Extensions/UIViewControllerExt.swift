ffa4ac9543356b63230b3712b023d940fc6fa140
vladimir <nepeyvodava@gmail.com>
2019-11-16 19:24:51 +0300

    UIViewController: added extension

--- NASA/Helper/Extensions/UIViewControllerExt.swift
+++ NASA/Helper/Extensions/UIViewControllerExt.swift
@@ -1,9 +1,13 @@
-//
-//  UIViewControllerExt.swift
-//  NASA
-//
-//  Created by Владимир on 15/11/2019.
-//  Copyright © 2019 nva. All rights reserved.
-//
+import UIKit
 
-import Foundation
+//MARK: - Creating and presenting alert
+extension UIViewController {
+        
+    func showAlert(error: Error) {
+        let alertVC = UIAlertController(title: "Error", message: error.localizedDescription, preferredStyle: .alert)
+        
+        alertVC.addAction(UIAlertAction(title: "Ok", style: .default, handler: nil))
+        present(alertVC, animated: true, completion: nil)
+    }
+    
+}
